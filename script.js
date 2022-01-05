f4f0bb65f12132ece09e21d88b912798f4857f30
student <abdullahnazari2001@gmail.com>
2022-01-05 16:48:29 +0600

    the scalling of the page is done

---
+++ script.js
@@ -0,0 +1,18 @@
+const WORLD_HEIGHT = 30;
+const WORLD_WIDTH = 100;
+const worldEl = document.querySelector("[data-world]");
+
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
+
+function setPixelToWorldScale() {
+  let worldToPixelScale;
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+  } else {
+    (worldToPixelScale = window), innerHeight / WORLD_HEIGHT;
+  }
+
+  worldEl.style.height = `${worldToPixelScale * WORLD_HEIGHT}px`;
+  worldEl.style.width = `${worldToPixelScale * WORLD_WIDTH}px`;
+}
