a901c2106f60c87782666bd44512d60baa0aee81
student <abdullahnazari2001@gmail.com>
2022-01-06 22:22:47 +0600

    icon added and update loop setup

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ const worldEl = document.querySelector("[data-world]");
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
 
+// Update Loop: to update postion of very element on the screen based on the time frame recieved from the monitor
+let lastTime;
+function update(time) {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
+  }
+  const delta = time - lastTime;
+  // console.log(delta);
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+window.requestAnimationFrame(update);
+
+//  setting the scale of the page on resize action
 function setPixelToWorldScale() {
   let worldToPixelScale;
   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
