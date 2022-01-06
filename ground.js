ab0b9dd7558711b6f1f0f0eeeb56132159af941b
student <abdullahnazari2001@gmail.com>
2022-01-06 23:50:58 +0600

    the score and start text are setup

--- ground.js
+++ ground.js
@@ -5,16 +5,16 @@ import {
 } from "./updateCustomProperty.js";
 
 const groundElems = document.querySelectorAll("[data-ground]");
-const Speed = 0.5;
+const Speed = 0.05;
 
 export function setupGround() {
   setCustomProperty(groundElems[0], "--left", 0);
   setCustomProperty(groundElems[1], "--left", 300);
 }
 
-export function ground(delta) {
+export function ground(delta, SpeedScale) {
   groundElems.forEach((ground) => {
-    incrementCustomProperty(ground, "--left", delta * Speed * -1);
+    incrementCustomProperty(ground, "--left", delta * Speed * SpeedScale * -1);
     if (getCustomProperty(ground, "--left") <= -300) {
       setCustomProperty(ground, "--left", 300);
     }
--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 import { ground, setupGround } from "./ground.js";
 
+// the constants
 const WORLD_HEIGHT = 30;
 const WORLD_WIDTH = 100;
-const worldEl = document.querySelector("[data-world]");
+const SPEED_SCALE = 0.001;
 
+// the dom elements
+const worldEl = document.querySelector("[data-world]");
+const startEl = document.querySelector("[data-start]");
+const scoreElem = document.querySelector("[data-score]");
+
+// initializers
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart);
 
 // Update Loop: to update postion of very element on the screen based on the time frame recieved from the monitor
-setupGround();
 let lastTime;
+let score = 0;
+
 function update(time) {
   if (lastTime == null) {
     lastTime = time;
@@ -17,12 +26,28 @@ function update(time) {
     return;
   }
   const delta = time - lastTime;
-  // console.log(delta);
   lastTime = time;
-  ground(delta);
+
+  // calling the ground loop
+  ground(delta, SPEED_SCALE);
   window.requestAnimationFrame(update);
+  // calling Update score
+  updateScore(delta);
+  scoreElem.textContent = score;
 }
-window.requestAnimationFrame(update);
+
+// start the key via a key press
+function handleStart() {
+  lastTime = null;
+  setupGround();
+  window.requestAnimationFrame(update);
+  startEl.classList.add("hide");
+}
+
+// updating the score based on the delta
+function updateScore(delta) {
+  return (score += Math.floor(delta * 0.06));
+}
 
 //  setting the scale of the page on resize action
 function setPixelToWorldScale() {
