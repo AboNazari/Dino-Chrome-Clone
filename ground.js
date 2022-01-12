9b55ee56fd647c05c3c4f7ad47709c4b521e0361
Abdullah Nazari <abdullahnazari2001@gmail.com>
2022-01-13 00:32:39 +0600

    Update ground.js

--- ground.js
+++ ground.js
@@ -9,7 +9,7 @@ const groundElems = document.querySelectorAll("[data-ground]");
 
 export function setupGround() {
   setCustomProperty(groundElems[0], "--left", 0);
-  setCustomProperty(groundElems[1], "--left", 300);
+  setCustomProperty(groundElems[1], "--left" , 300);
 }
 
 export function updateGround(delta, SpeedScale) {
